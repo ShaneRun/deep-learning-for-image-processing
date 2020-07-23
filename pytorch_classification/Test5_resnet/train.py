1d10c2bb51743b0aca8eed622be842acd75060e1
ShaneRun <chenshenpe@163.com>
2020-07-23 21:53:10 +0800

    final version on acceleration try

--- pytorch_classification/Test5_resnet/train.py
+++ pytorch_classification/Test5_resnet/train.py
@@ -9,42 +9,6 @@ from model import resnet34, resnet101
 import time
 
 
-class data_prefetcher():
-    def __init__(self, loader):
-        self.loader = iter(loader)
-        self.stream = torch.cuda.Stream()
-        self.mean = torch.tensor([0.485 * 255, 0.456 * 255, 0.406 * 255]).cuda().view(1, 3, 1, 1)
-        self.std = torch.tensor([0.229 * 255, 0.224 * 255, 0.225 * 255]).cuda().view(1, 3, 1, 1)
-        # With Amp, it isn't necessary to manually convert data to half.
-        # if args.fp16:
-        #     self.mean = self.mean.half()
-        #     self.std = self.std.half()
-        self.preload()
-
-    def preload(self):
-        try:
-            self.next_input, self.next_target = next(self.loader)
-        except StopIteration:
-            self.next_input = None
-            self.next_target = None
-            return
-        with torch.cuda.stream(self.stream):
-            self.next_input = self.next_input.cuda(non_blocking=True)
-            self.next_target = self.next_target.cuda(non_blocking=True)
-            # With Amp, it isn't necessary to manually convert data to half.
-            # if args.fp16:
-            #     self.next_input = self.next_input.half()
-            # else:
-            self.next_input = self.next_input.float()
-            self.next_input = self.next_input.sub_(self.mean).div_(self.std)
-
-    def next(self):
-        torch.cuda.current_stream().wait_stream(self.stream)
-        input = self.next_input
-        target = self.next_target
-        self.preload()
-        return input, target
-
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
@@ -92,7 +56,7 @@ validate_dataset = datasets.ImageFolder(root=image_path + "val",
 val_num = len(validate_dataset)
 validate_loader = torch.utils.data.DataLoader(validate_dataset,
                                               batch_size=batch_size, shuffle=False,
-                                              num_workers=0)
+                                              num_workers=12)
 
 net = resnet34()
 # load pretrain weights
@@ -116,7 +80,7 @@ for epoch in range(4):
     # train
     net.train()
     running_loss = 0.0
-    '''
+
     for step, data in enumerate(train_loader, start=0):
         images, labels = data
         optimizer.zero_grad()
@@ -132,34 +96,12 @@ for epoch in range(4):
         a = "*" * int(rate * 50)
         b = "." * int((1 - rate) * 50)
         print("\rtrain loss: {:^3.0f}%[{}->{}]{:.4f}".format(int(rate * 100), a, b, loss), end="")
-    '''
-    prefetcher = data_prefetcher(train_loader)
-    data, label = prefetcher.next()
-    iteration = 0
-    while data is not None:
-        iteration += 1
-        # 训练代码
-        #images, labels = data
-        optimizer.zero_grad()
-        logits = net(data.to(device))
-        loss = loss_function(logits, label.to(device))
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        # print train process
-        rate = (iteration + 1) / len(train_loader)
-        a = "*" * int(rate * 50)
-        b = "." * int((1 - rate) * 50)
-        print("\rtrain loss: {:^3.0f}%[{}->{}]{:.4f}".format(int(rate * 100), a, b, loss), end="")
-        data, label = prefetcher.next()
     print()
 
     # validate
     net.eval()
     acc = 0.0  # accumulate accurate number / epoch
-    '''
+
     with torch.no_grad():
         for val_data in validate_loader:
             val_images, val_labels = val_data
@@ -173,20 +115,6 @@ for epoch in range(4):
             torch.save(net.state_dict(), save_path)
         print('[epoch %d] train_loss: %.3f  test_accuracy: %.3f' %
               (epoch + 1, running_loss / step, val_accurate))
-    '''
-    with torch.no_grad():
-        for val_data in validate_loader:
-            val_images, val_labels = val_data
-            outputs = net(val_images.to(device))  # eval model only have last output layer
-            # loss = loss_function(outputs, test_labels)
-            predict_y = torch.max(outputs, dim=1)[1]
-            acc += (predict_y == val_labels.to(device)).sum().item()
-        val_accurate = acc / val_num
-        if val_accurate > best_acc:
-            best_acc = val_accurate
-            torch.save(net.state_dict(), save_path)
-        print('[epoch %d] train_loss: %.3f  test_accuracy: %.3f' %
-              (epoch + 1, running_loss / iteration, val_accurate))
 
 print('Finished Training')
 # print end time
